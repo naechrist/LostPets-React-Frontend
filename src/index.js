7564dd5f26f8b71a08ef5150780e4c4d5cd1d9d7
naechrist <729jenae.christensen@gmail.com>
2020-10-13 15:16:11 -0400

    created our store and set up index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createStore, applyMiddleware } from "redux"; //set up our store
+import { createStore, applyMiddleware, compose } from "redux"; // compose combines multiple middlewares into one so we can just pass in that one middleware in the store argument
 import thunk from "redux-thunk"; //for our async fetch requests
 import { Provider } from "react-redux"; //everything wraped in provider will have access to our redux store
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk))); //set up our store
+//store is where you store your data globally, reducer tells us what we want to do w the store based on the actions
+// reducer returns the new verison of the store
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
