fac23bf3e177e203946d992467cc254e33a2c1ee
Jenae Christensen <729jenae.christensen@gmail.com>
2020-12-07 16:48:48 -0500

    Update addPet.js

--- src/actions/addPet.js
+++ src/actions/addPet.js
@@ -1,7 +1,5 @@
 export const addPet = (pet, petTypeId) => {
-  console.log("b");
   return (dispatch) => {
-    console.log("c");
     fetch(
       `https://lost-pets-rails-backend.herokuapp.com/api/v1/pet_types/${petTypeId}/pets`,
       {
@@ -14,13 +12,10 @@ export const addPet = (pet, petTypeId) => {
     )
       .then((resp) => resp.json())
       .then((pet_type) => {
-        console.log("d");
         return dispatch({
           type: "ADD_PET",
           payload: pet_type,
         });
       });
-    console.log("e");
   };
-  console.log("f");
 };
